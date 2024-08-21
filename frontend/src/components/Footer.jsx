eef71cd841fb234a2b9c14f4e22df6db9f8f571b
Aditya Gupta <realadityagupta2@gmail.com>
2024-08-21 15:49:05 +0530

    completed the footer UI

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,14 @@
-import { Footer } from 'flowbite-react'
-import React from 'react'
-import { Link } from 'react-router-dom';
+import { Footer } from "flowbite-react";
+import React from "react";
+import { Link } from "react-router-dom";
+import { BsFacebook, BsInstagram, BsTwitter, BsGithub } from "react-icons/bs";
 
 const FooterCom = () => {
   return (
     <Footer container className="border border-t-8 border-teal-500">
-      <div className='w-full max-w-7xl mx-auto'>
-        <div className='grid w-full justify-between sm:flex md:grid-cols-1'>
-          <div className='mt-5 '>
+      <div className="w-full max-w-7xl mx-auto">
+        <div className="grid w-full justify-between sm:flex md:grid-cols-1">
+          <div className="mt-5 ">
             <Link
               to="/"
               className="self-center whitespace-nowrap text-lg sm:text-xl font-semibold dark:text-white"
@@ -61,29 +62,38 @@ const FooterCom = () => {
             <div>
               <Footer.Title title="Legal" />
               <Footer.LinkGroup col>
-                <Footer.Link
-                  href="https://github.com/coderAditya12"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
                   Privacy Policy
                 </Footer.Link>
 
-                <Footer.Link
-                  href="https://x.com/AdityaG2043097"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
+                <Footer.Link href="#" target="_blank" rel="noopener noreferrer">
                   Terms & condition
                 </Footer.Link>
               </Footer.LinkGroup>
             </div>
+          </div>
+        </div>
+        <Footer.Divider />
+        <div className="w-full sm:flex sm:items-center sm:justify-between">
+          <Footer.Copyright
+            href="#"
+            by="BlogTalk"
+            year={new Date().getFullYear()}
+          />
 
+          <div className="flex gap-6 sm:mt-0 mt-4 sm:justify-center">
+            <Footer.Icon href="#" icon={BsFacebook} />
+            <Footer.Icon href="#" icon={BsInstagram} />
+            <Footer.Icon href="https://x.com/AdityaG2043097" icon={BsTwitter} />
+            <Footer.Icon
+              href="https://github.com/coderAditya12"
+              icon={BsGithub}
+            />
           </div>
         </div>
       </div>
     </Footer>
   );
-}
+};
 
 export default FooterCom;

1a18c02d5f18a9d7d9cb893b55632ff6823a1034
Aditya Gupta <realadityagupta2@gmail.com>
2024-08-21 09:19:41 +0530

    connect frontend with backend and added signup functionality

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,8 +1,29 @@
 import { Button, Label, TextInput } from "flowbite-react";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const SignUp = () => {
+  const [formData, setFormData] = useState({});
+
+  const handleChange = (e) => {
+    setFormData({ ...formData, [e.target.id] : e.target.value });
+  };
+  console.log("Form Data:", formData);
+
+  const handleSubmit = async(e)=>{
+    e.preventDefault();
+    try {
+      const res = await fetch('/api/auth/signup',{
+        method:'POST',
+        headers:{'content-type':'application/json'},
+        body:JSON.stringify(formData),
+      });
+      const Data = await res.json();
+    } catch (error) {
+      
+    }
+  }
+
   return (
     <div className="min-h-screen mt-20">
       <div className="flex p-3 max-w-3xl mx-auto flex-col md:flex-row md:items-center gap-5">
@@ -20,26 +41,43 @@ const SignUp = () => {
         </div>
         {/* right side */}
         <div className="flex-1">
-          <form className="flex flex-col gap-2">
+          <form className="flex flex-col gap-2" onSubmit={handleSubmit}>
             <div>
               <Label value="Your username" />
-              <TextInput type="text" placeholder="username" id="username" />
+              <TextInput
+                type="text"
+                placeholder="username"
+                id="username"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Your email" />
-              <TextInput type="email" placeholder="name@company.com" id="email" />
+              <TextInput
+                type="email"
+                placeholder="name@company.com"
+                id="email"
+                onChange={handleChange}
+              />
             </div>
             <div>
               <Label value="Password" />
-              <TextInput type="password" placeholder="Password" id="Password" />
+              <TextInput
+                type="password"
+                placeholder="Password"
+                id="password"
+                onChange={handleChange}
+              />
             </div>
-            <Button gradientDuoTone='purpleToPink' type='submit'>Sign Up</Button>
+            <Button gradientDuoTone="purpleToPink" type="submit">
+              Sign Up
+            </Button>
           </form>
           <div className="flex gap-2 text-sm mt-5 ">
-            <span>
-              Have an account?
-            </span>
-            <Link to='/sign-in' className="text-blue-500">Sign In</Link>
+            <span>Have an account?</span>
+            <Link to="/sign-in" className="text-blue-500">
+              Sign In
+            </Link>
           </div>
         </div>
       </div>

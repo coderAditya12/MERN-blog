0684affeb80dd6b56285d6c4d48ba3fc23856b12
Aditya Gupta <realadityagupta2@gmail.com>
2024-08-22 08:31:08 +0530

    sign in logic and added error handling.

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const user = require("../models/user");
 const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 const { errorHandler } = require("../utils/error");
 const signUp = async (req, res, next) => {
   try {
@@ -32,4 +33,32 @@ const signUp = async (req, res, next) => {
     next(error);
   }
 };
-module.exports = { signUp };
+
+const signIn = async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password || email === "" || password === "") {
+    next(errorHandler(400, "All fields are required"));
+  }
+  try {
+    const validUser = await user.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "user not found"));
+    }
+
+    const validPassword = bcrypt.compare(password, validUser.password);
+    if (!validPassword) {
+      return next(errorHandler(404, "Invalid password"));
+    }
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    //sending the data without password
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .status(200)
+      .cookie("access_token", token, {
+        httpOnly: true,
+      })
+      .json(rest);
+  } catch (error) {}
+};
+module.exports = { signUp, signIn };
--- api/routes/auth.route.js
+++ api/routes/auth.route.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
-const { signUp } = require("../controllers/auth.controller");
-
+const { signUp,signIn } = require("../controllers/auth.controller");
 router.post("/signup", signUp);
+router.post("/signin", signIn);
 
 module.exports = router;

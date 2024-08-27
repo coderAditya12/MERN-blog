11d3b7fd1787ce86f7251dbb80bed830ffe0dec2
Aditya Gupta <realadityagupta2@gmail.com>
2024-08-27 19:08:00 +0530

    minor bug fixed

--- frontend/src/components/OAuth.jsx
+++ frontend/src/components/OAuth.jsx
@@ -1,22 +1,23 @@
 import { Button } from "flowbite-react";
 import React from "react";
-
 import { AiFillGoogleCircle } from "react-icons/ai";
 import { getAuth, GoogleAuthProvider, signInWithPopup } from "firebase/auth";
 import { app } from "../firebase";
+
 const OAuth = () => {
-  const auth = getAuth(app);
   const handleGoogleClick = async () => {
+    const auth = getAuth(app);
     const provider = new GoogleAuthProvider();
     provider.setCustomParameters({ prompt: "select_account" });
 
     try {
       const result = await signInWithPopup(auth, provider);
-      // console.log(result)
+      console.log(result);
     } catch (error) {
       console.log("error", error);
     }
   };
+
   return (
     <Button
       type="button"

a20c7bcc378be76bcbb72330fbca4ec4fefe3407
Aditya Gupta <realadityagupta2@gmail.com>
2024-08-29 19:12:22 +0530

    console error fixed

--- frontend/src/components/DashSidebar.jsx
+++ frontend/src/components/DashSidebar.jsx
@@ -1,33 +1,46 @@
 import React, { useEffect, useState } from "react";
 import { Sidebar } from "flowbite-react";
 import { HiArrowSmRight, HiUserCircle } from "react-icons/hi";
-import { Link, useLocation } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
+
 const DashSidebar = () => {
   const location = useLocation();
-  const [tab, setTab] = useState();
+  const navigate = useNavigate();
+  const [tab, setTab] = useState("");
+
   useEffect(() => {
     const urlParams = new URLSearchParams(location.search);
-    const tabFormUrl = urlParams.get("tab");
-    if (tabFormUrl) {
-      setTab(tabFormUrl);
+    const tabFromUrl = urlParams.get("tab");
+    if (tabFromUrl) {
+      setTab(tabFromUrl);
     }
   }, [location.search]);
+
+  const handleItemClick = (tabName) => {
+    navigate(`/dashboard?tab=${tabName}`);
+  };
+
   return (
     <Sidebar className="w-full md:w-56">
       <Sidebar.Items>
         <Sidebar.ItemGroup>
-          <Link to="/dashboard?tab=profile">
-            <Sidebar.Item
-              active={tab === "profile"}
-              icon={HiUserCircle}
-              label={"User"}
-              labelColor="dark"
-            >
-              Profile
-            </Sidebar.Item>
-          </Link>
+          <Sidebar.Item
+            active={tab === "profile"}
+            icon={HiUserCircle}
+            onClick={() => handleItemClick("profile")}
+            as="div"
+          >
+            Profile
+          </Sidebar.Item>
 
-          <Sidebar.Item icon={HiArrowSmRight} className="cursor-pointer">
+          <Sidebar.Item
+            icon={HiArrowSmRight}
+            className="cursor-pointer"
+            onClick={() => {
+              /* Handle sign out logic here */
+            }}
+            as="div"
+          >
             Sign Out
           </Sidebar.Item>
         </Sidebar.ItemGroup>

76b3df9fa2e60e99d4c3d872a39eb2eabcea0e99
Aditya Gupta <realadityagupta2@gmail.com>
2024-10-19 16:52:01 +0530

    add call to action to the post page

--- frontend/src/components/CallToAction.jsx
+++ frontend/src/components/CallToAction.jsx
@@ -2,10 +2,10 @@ import React from "react";
 import { Button } from "flowbite-react";
 const CallToAction = () => {
   return (
-    <div>
-      <div className="">
-        <h2>want to learn more about javascript ?</h2>
-        <p>Checkout these resources with 100 javascript Projects</p>
+    <div className="flex flex-col sm:flex-row p-3 border border-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+      <div className="flex-1 justify-center flex flex-col">
+        <h2 className="text-2xl ">want to learn more about javascript ?</h2>
+        <p className="text-gray-500 my-2">Checkout these resources with 100 javascript Projects</p>
         <Button
           gradientDuoTone="purpleToPink"
           className="rounded-tl-xl rounded-bl-none"
@@ -19,9 +19,9 @@ const CallToAction = () => {
           </a>
         </Button>
       </div>
-      <div className="p-7 ">
+      <div className="p-7 w-2/3">
         <img
-          src="https://user-images.githubusercontent.com/68542775/167072911-dc31eac8-6885-4a05-9c25-279ecce22a79.png"
+          src="https://user-images.githubusercontent.com/68542775/167072911-dc31eac8-6885-4a05-9c25-279ecce22a79.png" className="object-cover"
           alt=""
         />
       </div>
